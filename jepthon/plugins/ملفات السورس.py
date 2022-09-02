73fa63c25f551ab734d0075d50533f6440a8a32a
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-09-02 21:29:22 +0300

    Update ملفات السورس.py

--- jepthon/plugins/ملفات السورس.py
+++ jepthon/plugins/ملفات السورس.py
@@ -5,41 +5,6 @@ from ..helpers.utils import _catutils, parse_pre, yaml_format
 
 plugin_category = "tools"
 
-
-@jepiq.ar_cmd(
-    pattern="الملفات$",
-    command=("الملفات", plugin_category),
-    info={
-        "header": "To list all plugins in jepthon.",
-        "usage": "{tr}plugins",
-    },
-)
-async def _(event):
-    "To list all plugins in jepthon"
-    cmd = "ls jepthon/plugins"
-    o = (await _catutils.runcmd(cmd))[0]
-    OUTPUT = f"**[جيـبثون](tg://need_update_for_some_feature/) الـمـلفـات:**\n{o}"
-    await edit_or_reply(event, OUTPUT)
-
-
-@jepiq.ar_cmd(
-    pattern="فاراتي$",
-    command=("فاراتي", plugin_category),
-    info={
-        "header": "To list all environment values in jepthon.",
-        "description": "to show all heroku vars/Config values in your jepthon",
-        "usage": "{tr}env",
-    },
-)
-async def _(event):
-    "To show all config values in jepthon"
-    cmd = "env"
-    o = (await _catutils.runcmd(cmd))[0]
-    OUTPUT = (
-        f"**[جيـبثون](tg://need_update_for_some_feature/) قـائمـة الـفـارات:**\n\n\n{o}"
-    )
-    await edit_or_reply(event, OUTPUT)
-
 @jepiq.ar_cmd(
     pattern="متى$",
     command=("متى", plugin_category),
